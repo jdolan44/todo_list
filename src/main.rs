115250dd73978dcf6a508ae94570cfbb9b6b6c6c
jdolan44 <Jdolan16646@gmail.com>
2024-06-13 21:10:31 -0400

    user can check/uncheck tasks

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,27 @@ fn read_string() -> String{
     return String::from(input.trim());
 }
 
+fn read_index(max: usize) -> usize{
+    loop{
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line!");
+        let input: usize = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("invalid input! try again."); 
+                continue;
+            }
+        };
+        if input >= max {
+            println!("not a valid index!");
+            continue;
+        }
+        return input;
+    }
+}
+
 fn print_tasks(tasks: &Vec<Task>){
     println!("My tasks:");
     for task in tasks{
@@ -46,10 +67,22 @@ fn add_task(tasks: &mut Vec<Task>){
     tasks.push(task);
 }
 
+
+fn check_task(tasks: &mut Vec<Task>){
+    //TODO: finish this
+    print_tasks(tasks);
+    println!("Enter the task index you want to change:");
+    let index = read_index(tasks.len());
+
+    let task_to_change = &mut tasks[index];
+    task_to_change.completed = !task_to_change.completed;
+}
+
 fn display_options(){
     println!("\n");
     println!("+: add a task");
     println!("d: display all tasks");
+    println!("c: check/uncheck a task");
     println!("q: quit");
 }
 
@@ -63,6 +96,7 @@ fn main() {
         match choice.trim(){
             "+" => add_task(&mut tasks),
             "d" => print_tasks(&tasks),
+            "c" => check_task(&mut tasks),
             "o" => display_options(),
             "q" => break,
             _ => println!("Invalid option, try again!")
